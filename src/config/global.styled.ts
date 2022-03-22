cf416ea12642ec098682c6c7df332844e485f238
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-03-21 23:23:01 -0300

    Criação dos arquivoss de estilo global

---
+++ src/config/global.styled.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+    :root{
+        --login-background:#F0F0F7;
+        --login-h1: #32264D;
+        --login-p:#6A6180;
+        --login-font-input:#9C98A6;
+        --login-border-input:#E6E6F0;
+        --login-background-input:#FAFAFC;
+        --login-button: #F25D27;
+        --login-button-font: #fff;
+        --login-icone-fundo: #41414C;
+    }
+`;
