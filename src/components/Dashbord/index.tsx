19a1de2037a2ada0a99c4ba1681ab947dbbe4aad
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-03-31 14:34:09 -0300

    Dando uma edição no card

--- src/components/Dashbord/index.tsx
+++ src/components/Dashbord/index.tsx
@@ -4,12 +4,48 @@ import { Dashborder } from "./styles";
 export default function Dashboard() {
   return (
     <Dashborder>
-      <Card titulo="gostack-desafio-conceitos-react-native" num={1} descricao="Descrição do repositório" nome="Zairo" stars={5}/>
-      <Card titulo="gostack-desafio-conceitos-react-native" num={2} descricao="Descrição do repositório" nome="Zairo" stars={5}/>
-      <Card titulo="gostack-desafio-conceitos-react-native" num={3} descricao="Descrição do repositório" nome="Zairo" stars={5}/>
-      <Card titulo="gostack-desafio-conceitos-react-native" num={4} descricao="Descrição do repositório" nome="Zairo" stars={5}/>
-      <Card titulo="gostack-desafio-conceitos-react-native" num={5} descricao="Descrição do repositório" nome="Zairo" stars={5}/>
-      <Card titulo="gostack-desafio-conceitos-react-native" num={6} descricao="Descrição do repositório" nome="Zairo" stars={5}/>
+      <Card 
+        titulo="gostack-desafio-conceitos-react-native" 
+        num={1} 
+        descricao="Descrição do repositório" 
+        nome="Zairo" 
+        stars={5}
+      />
+      <Card 
+        titulo="gostack-desafio-conceitos-react-native" 
+        num={2} 
+        descricao="Descrição do repositório" 
+        nome="Zairo" 
+        stars={5}
+      />
+      <Card 
+        titulo="gostack-desafio-conceitos-react-native" 
+        num={3} 
+        descricao="Descrição do repositório" 
+        nome="Zairo" 
+        stars={5}
+      />
+      <Card 
+        titulo="gostack-desafio-conceitos-react-native" 
+        num={4} 
+        descricao="Descrição do repositório" 
+        nome="Zairo" 
+        stars={5}
+      />
+      <Card 
+        titulo="gostack-desafio-conceitos-react-native" 
+        num={5} 
+        descricao="Descrição do repositório" 
+        nome="Zairo" 
+        stars={5}
+      />
+      <Card 
+        titulo="gostack-desafio-conceitos-react-native" 
+        num={6} 
+        descricao="Descrição do repositório" 
+        nome="Zairo" 
+        stars={5}
+      />
     </Dashborder>
   );
 }
